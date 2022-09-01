c4adc6fcb96aa167a4f4d16df8abe113edd565df
Rojo5Amerike <rojo5@amerike.edu.mx>
2022-09-01 18:51:40 -0500

    Mi API instalación

---
+++ APIF747/APIF747/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace APIF747.Models
+{
+    public class TodoItem
+    {
+        public long TodoItemId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
--- APIF747/APIF747/Program.cs
+++ APIF747/APIF747/Program.cs
@@ -1,8 +1,20 @@
+using APIF747.Models;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
+//});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,8 +24,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
 }
 
 app.UseHttpsRedirection();
