c0edf69535182ab31ed50ca54452992732ca2a21
AmerikeAzul14 <nikot92089@upshopt.com>
2022-09-08 16:34:05 -0500

    Pre-postman use install http-repl

---
+++ APIF747/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace APIF747.Models
+{
+    public class TodoItem
+    {
+        public long TodoItemId { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
